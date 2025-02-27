373a1a5274f2ff87b1af4e6ec3778455d161cfed
52Hzooi <2993503066@qq.com>
2025-02-27 23:00:40 +0800

    C端菜品相关功能、菜品缓存

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -117,8 +117,12 @@ public class DishServiceImpl implements DishService {
     }
 
     @Override
-    public List<Dish> getByCategoryId(Long categoryId) {
-        List<Dish> dishes = dishMapper.getByCategoryId(categoryId);
+    public List<Dish> list(Long categoryId) {
+        Dish dish = Dish.builder()
+                .categoryId(categoryId)
+                .status(StatusConstant.ENABLE)
+                .build();
+        List<Dish> dishes = dishMapper.list(dish);
 //        List<DishVO> dishVOS = new ArrayList<>();
 //        for (int i = 0; i < dishes.size(); i++) {
 //            Dish dish = dishes.get(i);
@@ -154,6 +158,26 @@ public class DishServiceImpl implements DishService {
                 }
             }
         }
+    }
+
+    @Override
+    public List<DishVO> listWithFlavor(Dish dish) {
+        List<Dish> dishList = dishMapper.list(dish);
+
+        List<DishVO> dishVOList = new ArrayList<>();
+
+        for (Dish d : dishList) {
+            DishVO dishVO = new DishVO();
+            BeanUtils.copyProperties(d,dishVO);
+
+            //根据菜品id查询对应的口味
+            List<DishFlavor> flavors = dishFlavorMapper.getByDishId(d.getId());
+
+            dishVO.setFlavors(flavors);
+            dishVOList.add(dishVO);
+        }
+
+        return dishVOList;
     }
 
 }
